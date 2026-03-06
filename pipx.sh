65ee6763165b3d2f8d428c9a3f9e12b66be1a0ef
george <george>
2026-03-06 09:51:40 -0500

    first commit

--- pipx.sh
+++ pipx.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 [ -n "$1" ] && [ -n "$2" ] || exit
 name=$1
+prefix=$HOME/.local/share/pipx/$name
 shift
 set -v
-rm -rf ~/.local/share/pipx/$name
-/usr/bin/python3 -m venv ~/.local/share/pipx/$name
-~/.local/share/pipx/$name/bin/pip install $@
+rm -rf $prefix
+/usr/bin/python3 -m venv $prefix
+$prefix/bin/pip install $@
 cat > ~/.local/bin/$name <<EOF
 #!/bin/sh
-~/.local/share/pipx/$name/bin/$name \$@
+PATH="$prefix/bin:\$PATH"
+unset CONDA_PREFIX
+export VIRTUAL_ENV=$prefix
+$prefix/bin/$name \$@
 EOF
 chmod 755 ~/.local/bin/$name
